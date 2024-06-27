cdb96e1099adf754523c7408436aa6bed65518e5
Axel Niklasson <axel.niklasson@gmail.com>
2024-06-27 11:07:26 +0100

    60 s refresh, set cookie

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from bpresources import get_sessions
-from flask import Flask, render_template, make_response
+from flask import Flask, render_template, make_response, request
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms.fields import DateField, SubmitField
@@ -10,24 +10,28 @@ app.config['SECRET_KEY'] = 'secret'
 Bootstrap(app)
 
 class MyForm(FlaskForm):
-    date = DateField('Swim date', default=datetime.today())
+    date = DateField('Swim date')
     submit = SubmitField('Go')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    swim_date = datetime.today()
+    date_format = '%Y-%m-%d'
+    swim_date_str = request.cookies.get('swim_date')
+    if swim_date_str:
+        swim_date = datetime.strptime(swim_date_str, date_format)
+    else:
+        swim_date = datetime.today()
+
     sessions = get_sessions(swim_date)
     form = MyForm()
     if form.validate_on_submit():
         swim_date = form.data['date']
-        sessions = get_sessions(swim_date.strftime('%Y-%m-%d'))
-#        for session in s:
-#            start_time = session["starts_at"]["format_12_hour"]
-#            spaces = session["spaces"]
-    resp = make_response(render_template('index.html', form=form, data=sessions))
-    resp.set_cookie('swimdate', swim_date.strftime('%Y-%m-%d'))
+        sessions = get_sessions(swim_date)
+
+    resp = make_response(render_template('index.html', form=form, data={'sessions':sessions, 'swim_date':swim_date}))
+    resp.set_cookie('swim_date', swim_date.strftime(date_format))
     return resp
-#
+
 if __name__ == '__main__':
     app.run(debug=True)
 
